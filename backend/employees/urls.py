851abbb8f0f52d6036a71c8aaeaa3cf22822c435
savelij_t <tsavelij@gmail.com>
2024-11-30 15:27:53 +0300

    Implemented Emplyee params endpoints

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -9,4 +9,39 @@ urlpatterns = [
         # 'post': 'create',
     }), name='employees'),
 
+    # employees params
+    # -- functional_blocks
+    path('functional_blocks/', views.FunctionalBlockViewSet.as_view({
+        'get': 'list',
+    }), name='functional_blocks'),
+    # -- subdivisions_1
+    path('subdivisions_1/', views.Subdivision1ViewSet.as_view({
+        'get': 'list',
+    }), name='subdivisions_1'),
+    # -- subdivisions_2
+    path('subdivisions_2/', views.Subdivision2ViewSet.as_view({
+        'get': 'list',
+    }), name='subdivisions_2'),
+    # -- subdivisions_3
+    path('subdivisions_3/', views.Subdivision3ViewSet.as_view({
+        'get': 'list',
+    }), name='subdivisions_3'),
+    # -- subdivisions_4
+    path('subdivisions_4/', views.Subdivision4ViewSet.as_view({
+        'get': 'list',
+    }), name='subdivisions_4'),
+    # -- positions
+    path('positions/', views.PositionViewSet.as_view({
+        'get': 'list',
+    }), name='positions'),
+    # -- roles
+    path('roles/', views.RoleViewSet.as_view({
+        'get': 'list',
+    })),
+    # -- cities
+    path('cities/', views.CityViewSet.as_view({
+        'get': 'list',
+    })),
+
+
 ]
--- backend/employees/views.py
+++ backend/employees/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .models import Employee
-from .serializers import EmployeeSerializer
-
-
-# Create your views here.
+from .models import (Employee, FunctionalBlock, Subdivision1, Subdivision2,
+                     Subdivision3, Subdivision4, Position, Role, City)
+from .serializers import (EmployeeSerializer, FunctionalBlockSerializer, Subdivision1Serializer,
+                          Subdivision2Serializer, Subdivision3Serializer, Subdivision4Serializer,
+                          PositionSerializer, RoleSerializer, CitySerializer)
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     """
-    Вьюсет для работы с данными о сотрудниках
+    Вьюсет для работы с данными таблицы о сотрудниках
     """
 
     queryset = Employee.objects.all()
@@ -23,5 +23,121 @@ class EmployeeViewSet(viewsets.ModelViewSet):
         return super().list(request, *args, **kwargs)
 
 
+class FunctionalBlockViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о функциональных блоках
+    """
+
+    queryset = FunctionalBlock.objects.all()
+    serializer_class = FunctionalBlockSerializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список функциональных блоков
+        """
+        return super().list(request, *args, **kwargs)
 
 
+class Subdivision1ViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о "подразделениях 1"
+    """
+
+    queryset = Subdivision1.objects.all()
+    serializer_class = Subdivision1Serializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список подразделений 1
+        """
+        return super().list(request, *args, **kwargs)
+
+
+class Subdivision2ViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о "подразделениях 2"
+    """
+
+    queryset = Subdivision2.objects.all()
+    serializer_class = Subdivision2Serializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список подразделений 2
+        """
+        return super().list(request, *args, **kwargs)
+
+
+class Subdivision3ViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о "подразделениях 3"
+    """
+
+    queryset = Subdivision3.objects.all()
+    serializer_class = Subdivision3Serializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список подразделений 3
+        """
+        return super().list(request, *args, **kwargs)
+
+
+class Subdivision4ViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о "подразделениях 4"
+    """
+
+    queryset = Subdivision4.objects.all()
+    serializer_class = Subdivision4Serializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список подразделений 4
+        """
+        return super().list(request, *args, **kwargs)
+
+
+class PositionViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о позициях
+    """
+
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список позиций
+        """
+        return super().list(request, *args, **kwargs)
+
+
+class RoleViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о ролях
+    """
+
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список ролей
+        """
+        return super().list(request, *args, **kwargs)
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с данными таблицы о городах
+    """
+
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+    def list(self, request, *args, **kwargs):
+        """
+        Возвращает список городов
+        """
+        return super().list(request, *args, **kwargs)
